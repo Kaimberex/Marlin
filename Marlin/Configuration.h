5aad047488d89d1319ba4580b7855b6cc0f0b52a
Kaimberex <37189369+Kaimberex@users.noreply.github.com>
2023-03-27 16:24:39 -0500

    Probe Offset fixed and increase probe grid points.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1561,7 +1561,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -31.94, -40.66, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -31.94, -40.66, -2.24 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -2037,7 +2037,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 9
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
